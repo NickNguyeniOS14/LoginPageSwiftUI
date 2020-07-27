025204bd32a30b23e55e66405d32dda742765135
Nick <ptnguyen1901@gmail.com>
2020-07-26 21:42:53 -0400

    finish up UI

--- LoginPage/ContentView.swift
+++ LoginPage/ContentView.swift
@@ -11,11 +11,33 @@ let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255
 
 
 struct ContentView: View {
+  
+  @State var username = ""
+  @State var password = ""
     var body: some View {
       VStack {
         WelcomeText()
         UserImage()
+        TextField("Username",text:$username)
+          .padding()
+          .background(lightGreyColor)
+          .cornerRadius(5.0)
+          .padding(.bottom, 20)
+        // “ The SecureField needs the same arguments as a TextField. Therefore, we create another @State property called password and bind it to the SecureField”
+        
+        SecureField("Password",text:$password)
+          .padding()
+          .background(lightGreyColor)
+          .cornerRadius(5.0)
+          .padding(.bottom, 20)
+        Button(action: {
+          print("Button tapped")
+        }, label: {
+          LoginButtonContent()
+        })
+        
       }
+      .padding()
     }
 }
 
@@ -41,6 +63,24 @@ struct UserImage: View {
       .padding(.bottom, 75)
   }
 }
+
+
+struct LoginButtonContent: View {
+  var body: some View {
+    Text("LOGIN")
+      .font(.headline)
+      .foregroundColor(.white)
+      .padding()
+      .frame(width:220,height:60)
+      .background(Color.green)
+      .cornerRadius(15.0)
+  }
+}
+
+
+
+
+
 
 
 struct ContentView_Previews: PreviewProvider {
