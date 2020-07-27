9cd506393ab646b70c486b537c6faa818e03d2bb
Nick <ptnguyen1901@gmail.com>
2020-07-26 21:25:52 -0400

    Initial Commit

---
+++ LoginPage/LoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageApp.swift
+//  LoginPage
+//
+//  Created by Nick Nguyen on 7/26/20.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
