7f3fb943f192616251e1c7dc3cb2d7e947993364
Nick <ptnguyen1901@gmail.com>
2020-07-26 21:31:23 -0400

    set up

--- LoginPage/ContentView.swift
+++ LoginPage/ContentView.swift
@@ -7,14 +7,44 @@
 
 import SwiftUI
 
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0,opacity: 1.0)
+
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+      VStack {
+        WelcomeText()
+        UserImage()
+      }
     }
 }
 
+
+
+struct WelcomeText: View {
+  var body: some View {
+    Text("Welcome!")
+      .font(.largeTitle)
+      .fontWeight(.semibold)
+      .padding(.bottom, 20)
+  }
+}
+
+struct UserImage: View {
+  var body: some View {
+    Image("userImage")
+      .resizable()
+      .aspectRatio(contentMode: .fill)
+      .frame(width:150,height: 150)
+      .clipped()
+      .cornerRadius(150)
+      .padding(.bottom, 75)
+  }
+}
+
+
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
